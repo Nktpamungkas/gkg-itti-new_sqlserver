8e99aff6bd48c29115afaa740797a1e966b2554c
tobias.mikha <140148318+ITCProgrammer@users.noreply.github.com>
2024-09-20 10:07:13 +0700

    upload koneksi & utils gkg-itti-new

--- koneksi.php
+++ koneksi.php
@@ -7,10 +7,40 @@ $password = "Xou@RUnivV!6";
 $db_name = "TM";
 $connInfo = array("Database" => $db_name, "UID" => $username, "PWD" => $password);
 $conn = sqlsrv_connect($host, $connInfo);
-$con = mysqli_connect("10.0.0.10", "dit", "4dm1n", "db_ikg");
+// $con = mysqli_connect("10.0.0.10", "dit", "4dm1n", "db_ikg");
 //$con = mysqli_connect("localhost", "root", "", "db_ikg");
-$connn = mysqli_connect("10.0.0.10", "dit", "4dm1n", "dbnow_gkg");
-$conr = mysqli_connect("10.0.0.10", "dit", "4dm1n", "dbnow_gerobak");
+// $connn = mysqli_connect("10.0.0.10", "dit", "4dm1n", "dbnow_gkg");
+// $conr = mysqli_connect("10.0.0.10", "dit", "4dm1n", "dbnow_gerobak");
+
+// Koneksi ke SQLSERVER
+$hostSVR19 = "10.0.0.221";
+$usernameSVR19 = "sa";
+$passwordSVR19 = "Ind@taichen2024";
+$gkgitti = "db_ikg";
+$nowgkg ="dbnow_gkg";
+$nowgerobak = "dbnow_gerobak";
+
+$db_ikg = array("Database" => $gkgitti, "UID" => $usernameSVR19, "PWD" => $passwordSVR19);
+$dbnow_gkg = array("Database" => $nowgkg, "UID" => $usernameSVR19, "PWD" => $passwordSVR19);
+$dbnow_gerobak = array("Database" => $nowgerobak, "UID" => $usernameSVR19, "PWD" => $passwordSVR19);
+
+$con = sqlsrv_connect($hostSVR19, $db_ikg);
+$connn = sqlsrv_connect($hostSVR19, $dbnow_gkg);
+$conr = sqlsrv_connect($hostSVR19, $dbnow_gerobak);
+if ($con) {
+} else {
+    exit("SQLSVR19 db_ikg Connection failed");
+}
+if ($connn) {
+} else {
+    exit("SQLSVR19 dbnow_gkg Connection failed");
+}
+if ($conr) {
+} else {
+    exit("SQLSVR19 dbnow_gerobak Connection failed");
+}
+// End Con
+
 
 $hostname = "10.0.0.21";
 $database = "NOWPRD";
